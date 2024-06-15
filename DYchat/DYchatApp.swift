6ce0a5676b1591775183289e5287d234a11dc364
Romesh Singhabahu <rosin@digitalyogin.com>
2024-06-15 15:27:25 +0200

    Initial Commit

---
+++ DYchat/DYchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DYchatApp.swift
+//  DYchat
+//
+//  Created by Romesh Singhabahu on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct DYchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
